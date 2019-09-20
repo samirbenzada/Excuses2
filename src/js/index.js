bf85ccbfc26bac89068a1be53a5691a250008556
Samir Benzada <samirbenzada@gmail.com>
2019-09-20 21:14:32 +0000

    excuses done

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,30 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let eatWhat = ["my homework", "my backpack", "my lunch", "my money"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = this.Math.floor(Math.random() * who.length);
+  let randomWhat = this.Math.floor(Math.random() * what.length);
+  let randomEat = this.Math.floor(Math.random() * eatWhat.length);
+  let randomWhen = this.Math.floor(Math.random() * when.length);
+
+  let excuse =
+    who[randomWho] +
+    " " +
+    what[randomWhat] +
+    " " +
+    eatWhat[randomEat] +
+    " " +
+    when[randomWhen];
+
+  this.document.body.querySelector("#inject").innerHTML = excuse;
 };
